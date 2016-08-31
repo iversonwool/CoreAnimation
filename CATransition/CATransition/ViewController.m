2cab68f684eb1f0da1657bb028709e9f7ae5bae1
李浩 <LeeHow@lihaodemacbook-pro.local>
2016-08-31 11:14:49 +0800

    CATransition(转场动画)

---
+++ CATransition/CATransition/ViewController.m
@@ -0,0 +1,68 @@
+//
+//  ViewController.m
+//  CATransition
+//
+//  Created by 李浩 on 16/8/31.
+//  Copyright © 2016年 李浩. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imgView;
+@property (assign, nonatomic) NSUInteger count;
+@end
+
+@implementation ViewController
+
+// 这个 方法不会走
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.count = 1;
+        NSLog(@"%s, %lu", __func__, _count);
+    }
+    return self;
+}
+
+
+- (void)awakeFromNib {
+    self.count = 1;
+    NSLog(@"%s, %lu", __func__, _count);
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+//    self.count = 1;
+    
+//    NSLog(@"%s, %lu", __func__, _count);
+}
+
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    self.count++;
+
+    if (_count==4) {
+        self.count = 1;
+    }
+
+    self.imgView.image = [UIImage imageNamed:[NSString stringWithFormat:@"%lu", _count]];
+
+    CATransition *transiton = [CATransition animation];
+    // 动画方式
+    transiton.type = @"cube";
+    // 动画方式变化的方向
+    transiton.subtype = kCATransitionReveal;
+    // 动画时间
+//    transiton.duration = 2;
+//    transiton.endProgress
+    [self.imgView.layer addAnimation:transiton forKey:nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
