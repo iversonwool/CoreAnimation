d4839e12dc7d1740626f7f7cb729ef81603576a8
李浩 <286300785@qq.com>
2018-11-19 21:40:36 +0800

    animation

--- UIViewTransiton/UIViewTransiton/ViewController.m
+++ UIViewTransiton/UIViewTransiton/ViewController.m
@@ -6,9 +6,17 @@
 //  Copyright © 2016年 李浩. All rights reserved.
 //
 
+// UIViewPropertyAnimator
+
 #import "ViewController.h"
 
+#include <dlfcn.h>
+
 @interface ViewController ()
+
+@property (nonatomic, strong) UIView *redView;
+
+@property (nonatomic, strong) UIViewPropertyAnimator *propertyAnimator;
 
 @end
 
@@ -20,6 +28,35 @@
     
     
     
+    
+//
+    self.redView = [[UIView alloc] init];
+    [self.view addSubview:self.redView];
+    self.redView.frame = CGRectMake(50, 50, 100, 100);
+    self.redView.backgroundColor = [UIColor redColor];
+    
+    
+    
+    void (^animation)(void) = ^() {
+        self.redView.frame = CGRectMake(50, 50, 50, 50);
+    };
+    
+    self.propertyAnimator = [[UIViewPropertyAnimator alloc] initWithDuration:2 curve:UIViewAnimationCurveEaseInOut animations:animation];
+
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn setTitle:@"animator" forState:UIControlStateNormal];
+    [self.view addSubview:btn];
+    btn.frame = CGRectMake(10, 500, 100, 20);
+    btn.backgroundColor = [UIColor cyanColor];
+    [btn addTarget:self action:@selector(btnClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
+}
+
+- (void)btnClicked:(UIButton *)btn {
+    
+    NSLog(@"-----%@", self.propertyAnimator);
+    [self.propertyAnimator startAnimation];
 }
 
 - (void)didReceiveMemoryWarning {
